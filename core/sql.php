ccd46f97bc86a1f022c3587b42232004e3ca7a77
MatheusI <matheusinoue2006@gmail.com>
2023-08-15 12:22:25 -0300

    atualizado2

--- core/sql.php
+++ core/sql.php
@@ -1,69 +1,67 @@
 <?php
-    function insert (string $entidade, array $dados) : String 
+    function insert (string $entidade, array $dados): string
     {
-    $instrucao = "INSERT INTO {$entidade}";
+        $instrucao = "INSERT INTO {$entidade}";
 
-    $campos = implode(', ', array_keys($dados));
-    $valores = implode(', ', array_values($dados));
+        $campos = implode(',', array_keys($dados));
+        $valores = implode(',', array_values ($dados));
 
-    $instrucao.= "({$campos})";
-    $instrucao .= " VALUES ({$valores})";
-
-    return $instrucao;
-    }
-
-
-
-    function delete (string $entidade, array $criterio = []) : String
-    {
-        $instrucao = "DELETE FROM {$entidade}";
-
-        if(!empty($criterio)){
-            $instrucao .=' WHERE ';
-
-            foreach($criterio as $expressao){
-                $instrucao .=' ' . implode(' ', $expressao);
-            }
-        }
+        $instrucao .= " ({$campos})";
+        $instrucao .= " VALUES ({$valores})";
 
         return $instrucao;
     }
 
-    function update (string $entidade, array $dados, array $criterio = []) : string
-    {
+    function update (string $entidade, array $dados, array $criterio =  [] ): string
+    { 
         $instrucao = "UPDATE {$entidade}";
 
-        foreach ($dados as $campo => $dado)
-        {
+        foreach ($dados as $campo => $dado) {
             $set[] = "{$campo} = {$dado}";
         }
-        $instrucao .= ' SET ' . implode(',', $set) ;
 
-        if (!empty($criterio)){
-            $instrucao .= ' WHERE';
-            
-            foreach($criterio as $expressao) {
-                $instrucao .= ' '. implode (' ',$expressao);
+        $instrucao .= ' SET ' . implode(', ', $set);
+
+        if (!empty($criterio)) { 
+            $instrucao .= ' WHERE ';
+
+            foreach ($criterio as $expressao) {
+                $instrucao .= ' ' . implode(' ', $expressao);
             }
         }
+
         return $instrucao;
     }
 
-    function  select (string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
+    function delete(string $entidade, array $criterio = []) : string
     {
-        $instrucao = " SELECT " . implode(',',$campos);
-        $instrucao .= " FROM {$entidade}";
-
-        if(!empty($criterio)){
+        $instrucao = "DELETE FROM {$entidade}";
+        if(!empty($criterio)) {
             $instrucao .= ' WHERE ';
-
-            foreach ($criterio as $expressao){
-                $instrucao .= ' '.implode (' ', $expressao);
+            foreach ($criterio as $expressao) {
+                $instrucao .= ' ' . implode(' ', $expressao);
             }
         }
-        if(!empty($ordem)){
-            $instrucao .= " ORDER BY $ordem ";
-        }
+
         return $instrucao;
-}
+    }
+
+    function select(string $entidade, array $campos = ['*'], array $criterio = [], string $ordem = null) : string
+    {
+        $instrucao = "SELECT " . implode(', ', $campos);
+        $instrucao .= " FROM {$entidade}";
+
+        if(!empty($criterio)) {
+            $instrucao .= ' WHERE ';
+            foreach ($criterio as $expressao) {
+                $instrucao .= ' ' . implode(' ', $expressao);
+            }
+        }
+
+        if(!empty($ordem)) {
+            $instrucao .= " ORDER BY {$ordem}";
+        }
+
+        return $instrucao;
+    }
 ?>
--- tests/mysql_test.php
+++ tests/mysql_test.php
@@ -1,37 +1,41 @@
 <?php
-require_once '../includes/funcoes.php';
-require_once '../core/conexao_mysql.php';
-require_once '../core/sql.php';
-require_once '../core/mysql.php';
+    require_once '../includes/funcoes.php';
+    require_once '../core/conexao_mysql.php';
+    require_once '../core/sql.php';
+    require_once '../core/mysql.php';
 
-insert_teste ('inoue', 'inoue.m@aluno.ifsp.edu.br', 'japao135');
-buscar_teste();
-update_teste(38, 'murilo', 'silva@gmail.com');
-buscar_teste();
+    insert_teste('Inoue', 'inoue.m@aluno.ifsp.edu.br', 'japao135');
+    insert_teste('Inoue', 'inoue.m@aluno.ifsp.edu.br', 'japao135');
+    insert_teste('Inoue', 'inoue.m@aluno.ifsp.edu.br', 'japao135');
+    buscar_teste();
+    update_teste(1, 'murilo', 'silva@gmail.com');
+    buscar_teste();
+    deleta_teste(3);
 
-//Teste inserção banco de dados
-function insert_teste($nome, $email, $senha): void{
+    function insert_teste($nome, $email, $senha) : void
+    {
+        $dados = ['nome' => $nome
+                , 'email' => $email
+                , 'senha' => $senha];
+        insere('usuario', $dados);
+    }
 
-    $dados = ['nome' => $nome, 'email' => $email, 'senha' => $senha]; 
-    insere ('usuario', $dados);
-}
+    function buscar_teste() : void
+    {
+        $usuarios = buscar('usuario', ['id', 'nome', 'email'], [],'');
+        print_r($usuarios);
+    }
 
-//Teste select banco de dados
-function buscar_teste(): void{
-    $usuarios = buscar('usuario',['id', 'nome', 'email'], [], '');
-    print_r($usuarios);
-}
-
-//Teste update banco de dados
-
-function update_teste($id, $nome, $email): void{
-
-    $dados = ['nome' => $nome, 'email' => $email];
-    $criterio = [['id', '=', $id]];
-    atualiza('usuario', $dados, $criterio);
-}
-
-
-
-
+    function update_teste($id, $nome, $email) : void
+    {
+        $dados = ['nome' => $nome
+                , 'email' => $email];
+        $criterio = [['id', '=', $id]];
+        atualiza('usuario', $dados, $criterio);
+    }
+    function deleta_teste($id) : void
+    {
+        $criterio = [['id', '=', $id]];
+        deleta('usuario', $criterio);
+    }
 ?>
