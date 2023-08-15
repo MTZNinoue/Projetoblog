96bddc16e57b97bd022f8fd80f892354e8eb4788
MatheusI <matheusinoue2006@gmail.com>
2023-08-15 11:58:20 -0300

    atualizado

--- tests/mysql_test.php
+++ tests/mysql_test.php
@@ -4,7 +4,7 @@ require_once '../core/conexao_mysql.php';
 require_once '../core/sql.php';
 require_once '../core/mysql.php';
 
-insert_teste ('Rafael', 'r.reichenbach@aluno.ifsp.edu.br', 'ratin1234');
+insert_teste ('inoue', 'inoue.m@aluno.ifsp.edu.br', 'japao135');
 buscar_teste();
 update_teste(38, 'murilo', 'silva@gmail.com');
 buscar_teste();
